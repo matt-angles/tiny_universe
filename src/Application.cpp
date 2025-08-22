a964cf1a9930baffe14ed710a4728952b2e864a5
Matthew Anglès d'Auriac <matt.angles@proton.me>
2025-08-22 15:43:18 +0200

    Top-level

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,1 +1,33 @@
-int main() {}
+#include <exception>
+#include <iostream>
+#include <cstdlib>
+
+
+class Application {
+public:
+    Application() {}
+    void run() {}
+    ~Application() {}
+};
+
+
+int main()
+{
+    try
+    {
+        Application app = Application();
+        app.run();
+    }
+    catch (const std::exception& exception)
+    {
+        std::cerr << exception.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    catch (...)
+    {
+        std::cerr << "### An unknown error has occured!! ###" << std::endl;
+        return EXIT_FAILURE;
+    }
+    
+    return EXIT_SUCCESS;
+}
