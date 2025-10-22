e6216a9dcb0d2b3545f32c0321be9fa7dcbfa193
Matthew Anglès d'Auriac <matt.angles@proton.me>
2025-10-22 15:39:35 +0100

    Square mesh

--- src/renderer/Mesh.cpp
+++ src/renderer/Mesh.cpp
@@ -16,3 +16,28 @@ void Triangle::fill_bufs(Vector2D* vertBuf,  size_t& iVert,
     indexBuf[iIndex++] = iVert; vertBuf[iVert++] = b;
     indexBuf[iIndex++] = iVert; vertBuf[iVert++] = c;
 }
+
+
+Quad::Quad(Vector2D a, Vector2D b, Vector2D c, Vector2D d)
+  : a(a), b(b), c(c), d(d) {}
+std::unique_ptr<Mesh> Quad::clone() const
+{
+    return std::make_unique<Quad>(*this);
+}
+
+void Quad::fill_bufs(Vector2D* vertBuf,  size_t& iVert, 
+                     uint16_t* indexBuf, size_t& iIndex)
+{    
+    vertBuf[iVert]   = a; vertBuf[iVert+1] = b; 
+    vertBuf[iVert+2] = c; vertBuf[iVert+3] = d; 
+
+    indexBuf[iIndex++] = iVert;
+    indexBuf[iIndex++] = iVert+1;
+    indexBuf[iIndex++] = iVert+2;
+
+    indexBuf[iIndex++] = iVert+1;
+    indexBuf[iIndex++] = iVert+2;
+    indexBuf[iIndex++] = iVert+3;
+
+    iVert+=4;
+}
--- src/renderer/Mesh.hpp
+++ src/renderer/Mesh.hpp
@@ -27,3 +27,19 @@ private:
     Vector2D b;
     Vector2D c;
 };
+
+
+class Quad : public Mesh {
+public:
+    Quad(Vector2D a, Vector2D b, Vector2D c, Vector2D d);
+    std::unique_ptr<Mesh> clone() const override;
+
+    void fill_bufs(Vector2D* vertBuf,  size_t& iVert, 
+                   uint16_t* indexBuf, size_t& iIndex) override;
+
+private:
+    Vector2D a;
+    Vector2D b;
+    Vector2D c;
+    Vector2D d;
+};
